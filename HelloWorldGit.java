740e10d082f6a40846b92e39930acbdf432eef7a
Shital Jadhav <shital_jadhav@persistent.co.in>
2018-07-26 15:40:46 +0530

    added ............and master

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -10,7 +10,7 @@ System.out.println("Display Todays date:-");
 
 public static void main(String[] args)
 {
-System.out.println("Hello World ...Welcome to Git");
+System.out.println("Hello World ...Welcome to Git.......master");
 myMethod();
 
 }
